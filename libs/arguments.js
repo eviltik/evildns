a62bcf2f8455f04cdf7e8d259c829bc07b38aa3f
eviltik <eviltik@gmail.com>
2017-10-30 17:22:38 +0100

    remove unused file

--- libs/arguments.js
+++
@@ -1,9 +0,0 @@
-function checkArguments() {
-    if (!process.argv[2]) {
-        console.log('Usage: evildns path/to/cidr.txt');
-        process.exit();
-    }
-    return process.argv[2];
-}
-
-module.exports = checkArguments;
